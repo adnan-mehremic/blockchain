d729ecd048617d3a15bf9db04eb0bc874898dfdc
mehremic1 <mehremic.a@gmail.com>
2019-03-04 13:55:20 +0100

    add method for get previous block

--- blockchain.py
+++ blockchain.py
@@ -16,6 +16,9 @@ class Blockchain:
                  'previous_hash': previous_hash}
         self.chain.append(block)
         return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
         
         
         
