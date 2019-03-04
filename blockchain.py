022ac4f254f2ba0061aaaf16c63479679d52c86a
mehremic1 <mehremic.a@gmail.com>
2019-03-04 17:02:30 +0100

    add method for check proof of work

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 # Create blockchain
 
 import datetime
+import hashlib
 
 
 class Blockchain:
@@ -18,8 +19,17 @@ class Blockchain:
         return block
     
     def get_previous_block(self):
-        return self.chain[-1]
-        
-        
-        
-        
+        return self.chain[-1] 
+    
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(
+                    str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
